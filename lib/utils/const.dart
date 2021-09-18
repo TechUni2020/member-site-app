68ea9550c5ad1520c82a3d49f812b7e5a0a9e137
Yurika FUJIMOTO <>
2021-09-18 15:42:08 +0900

    :sparkles: add sign-in

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -4,10 +4,7 @@ class Const{
   // 色の保管庫
   static const utilColor = {
    "white": const Color(0xffffffff),
-   "gray": const Color.fromRGBO(97, 97, 97, 1),
-   "black": const Color.fromRGBO(128, 128, 128, 1),
-   "mild_black": const Color.fromRGBO(40, 45, 48, 1),
-   "green": const Color.fromRGBO(47, 64, 61, 1),
-   "lightgreen": const Color.fromRGBO(47, 64, 61, 0.9),
+   "grey": const Color.fromRGBO(115,115,115,1),
+   "lightblue": const Color.fromRGBO(200,228,224,1),
   };
 }
