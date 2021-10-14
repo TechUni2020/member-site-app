fbc3c66227d5c7e25d16b92d445266fa7e3a71c1
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-10-14 21:42:04 +0900

    firebaseのflgを操作できるようにした。見た目に反映はまだ

--- lib/screens/mypages/mypage.dart
+++ lib/screens/mypages/mypage.dart
@@ -50,6 +50,7 @@ class _mypageState extends State<mypage> {
     // flg = flgs.data()["flg"];
     // }
   }
+
   @override
   void initState() {
     super.initState();
@@ -64,12 +65,7 @@ class _mypageState extends State<mypage> {
     //   return x;
     // }
     String name = "福山祐史";
-    String department = "経済学部";
-    String part = "開発者";
-    String year = "3年生";
-    String hobby = "カメラ";
-    String likefood = "バナナ";
-    String dislikefood = "セロリ";
+
 
     // print("//////////////////////////");
     // print(user);
@@ -275,7 +271,7 @@ class _mypageState extends State<mypage> {
 
 
 
-    Widget maincard(String x, String y) {
+    Widget maincard(String x, String y,var z) {
       return Stack(
         children: <Widget>[
           Container(
@@ -323,19 +319,21 @@ class _mypageState extends State<mypage> {
             right: 30,
             width: 10.0,
             height: 10.0,
-            child: GestureDetector(
-              onTap: () {
-                // 動作を記述
-              },
-              child: Opacity(
-                opacity: 0.5,
-                child: GestureDetector(
-                  onTap: () {
-                    // 処理を書く
-                  },
-                  child: Icon(Icons.border_color // アイコンの色を設定できる
-                      ),
+            child: Opacity(
+              opacity: 0.5,
+              child: GestureDetector(
 
+                onTap: () {
+                  // instagramdialog(context);
+
+                  _firestore
+                      .collection('user')
+                      .doc(user.uid)
+                      .update({"flg": z}).then((_) {
+                    print("success!");
+                  });
+                },
+                child: Icon(Icons.border_color // アイコンの色を設定できる
                 ),
               ),
             ),
@@ -344,6 +342,7 @@ class _mypageState extends State<mypage> {
       );
     };
 
+    String inputvalue="";
     Widget editmaincard(String x) {
       return Stack(
         children: <Widget>[
@@ -380,35 +379,43 @@ class _mypageState extends State<mypage> {
                     padding: EdgeInsets.only(
                       top: 0, bottom: 0,
                     ),
-
                     child:TextField(
                       textAlign: TextAlign.center,
                       style: TextStyle(fontSize: 18.0),
+                      onChanged: (value) {
+                        inputvalue = value;
+                      },
                     ),
       ),
-
-                    Container(
-                          height: 30,
-                          // width: 60,
-                          padding: EdgeInsets.only(
-                            top: 10, bottom: 0,
-                          ),
-
-                          child:Container(
-                            alignment: Alignment.bottomRight,
-                            child:FlatButton(
-                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              color: Colors.white,
-                              textColor: Colors.blue,
-                              child: Text('OK'),
-                              onPressed: () {
-                                //OKを押したあとの処理
-                              },
-
+                  Container(
+                    height: 30,
+                    width: 100,
+                    padding: EdgeInsets.only(
+                      top: 0,
+                      bottom: 0,
+                    ),
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      padding: EdgeInsets.all(0.0),
+                      width: 30,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(Icons.check // アイコンの色を設定できる
                             ),
-
-                          ),
-
+                        onPressed: () {
+                          _firestore
+                              .collection('user')
+                              .doc(user.uid)
+                              .update({x: inputvalue});
+                          _firestore
+                              .collection('user')
+                              .doc(user.uid)
+                              .update({"flg": 0}).then((_) {
+                            print("success!");
+                          });
+                        },
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -429,7 +436,12 @@ class _mypageState extends State<mypage> {
     //     print("success!");
     //   });
     // }
-
+    // Color selectColor() {
+    //   if(personalinformation[6] == 0) {
+    //     return editmaincard("学部");
+    //   } else {
+    //     return Container(child: maincard("クラス",personalinformation[1]));
+    //   }
     final gathermaincard = Container(
       child: Container(
         child: Table(
@@ -441,19 +453,21 @@ class _mypageState extends State<mypage> {
           },
           children: [
             TableRow(children: [
-              editmaincard("学部"),
-              maincard("クラス",personalinformation[1]),
-              maincard("学年",personalinformation[2]),
+              flg[0] == 1 ? Container(child: editmaincard("学部")):Container(child: maincard("学部",personalinformation[0],1)),
+              flg[0] == 2 ? Container(child: editmaincard("クラス")):Container(child: maincard("クラス",personalinformation[1],2)),
+              flg[0] == 3 ? Container(child: editmaincard("学年")):Container(child: maincard("学年",personalinformation[2],3)),
             ]),
             TableRow(children: [
-              maincard("趣味",personalinformation[3]),
-              maincard("好きな食べ物",personalinformation[4]),
-              maincard("嫌いな食べも物",personalinformation[5]),
+              flg[0] == 4 ? Container(child: editmaincard("趣味")):Container(child: maincard("趣味",personalinformation[3],4)),
+              flg[0] == 5 ? Container(child: editmaincard("好きな食べ物")):Container(child: maincard("好きな食べ物",personalinformation[4],5)),
+              flg[0] == 6 ? Container(child: editmaincard("嫌いな食べも物")):Container(child: maincard("嫌いな食べも物",personalinformation[5],6)),
             ]),
           ],
         ),
       ),
     );
+
+
 
     final information = Container(
       child: Container(
@@ -495,6 +509,7 @@ class _mypageState extends State<mypage> {
     );
 
 
+
     final editbotton = ElevatedButton(
       child: const Text('編集画面へ'),
       style: ElevatedButton.styleFrom(
@@ -502,6 +517,37 @@ class _mypageState extends State<mypage> {
         onPrimary: Colors.white,
       ),
     );
+
+    Widget firebasecard() {
+      return StreamBuilder<QuerySnapshot>(
+        stream: _firestore
+          .collection('user')
+          .doc(user.uid)
+          .get(),
+
+
+            //streamが更新されるたびに呼ばれる
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot) {
+
+            //データが取れていない時の処理
+            if (!snapshot.hasData) return const Text('Loading...');
+
+            print("aa");
+            print(snapshot.data.docs);
+            // personalinformation[0]=snapshot.data.document[user.uid]["department"];
+            // personalinformation[1]=(snapshot.data.docs[user.uid]["class"]);
+            // personalinformation[2]=(snapshot.data.doc[user.uid]["grade"]);
+            // personalinformation[3]=(snapshot.data.doc[user.uid]["hobby"]);
+            // personalinformation[4]=(snapshot.data.doc[user.uid]["likefood"]);
+            // personalinformation[5]=(snapshot.data.doc[user.uid]["dislikefood"]);
+            // personalinformation[6]=(snapshot.data.doc[user.uid]["information"]);
+            return gathermaincard;
+        }
+      );
+    }
+    firebasecard();
+
 
     return Scaffold(
         // appBar: appBarMain(context),
@@ -549,8 +595,32 @@ class _mypageState extends State<mypage> {
                 ),
                   child: Column(
                     children: <Widget>[
+                //   StreamBuilder<QuerySnapshot>(
+                //   stream: _firestore
+                //       .collection('user')
+                //       .snapshots(),
+                //
+                //     //streamが更新されるたびに呼ばれる
+                //     builder: (BuildContext context,
+                //         AsyncSnapshot<QuerySnapshot> snapshot) {
+                //
+                //       //データが取れていない時の処理
+                //       if (!snapshot.hasData) return const Text('Loading...');
+                //
+                //       print("aa");
+                //       print(snapshot.data.document());
+                //       // personalinformation[0]=snapshot.data.document[user.uid]["department"];
+                //       // personalinformation[1]=(snapshot.data.docs[user.uid]["class"]);
+                //       // personalinformation[2]=(snapshot.data.doc[user.uid]["grade"]);
+                //       // personalinformation[3]=(snapshot.data.doc[user.uid]["hobby"]);
+                //       // personalinformation[4]=(snapshot.data.doc[user.uid]["likefood"]);
+                //       // personalinformation[5]=(snapshot.data.doc[user.uid]["dislikefood"]);
+                //       // personalinformation[6]=(snapshot.data.doc[user.uid]["information"]);
+                //       return gathermaincard;
+                //     }
+                // ),
                       gathermaincard,
-                    ],
+          ],
                   ),
                 ),
                 Container(
