8c9b78c8a591de92f8fdfb811b3b4fe2c9fefa18
井手翔陽 <shouhi605@gmail.com>
2022-02-20 12:33:56 +0900

    add Database Services

--- lib/Services/DatabaseServices.dart
+++ lib/Services/DatabaseServices.dart
@@ -1,4 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:techuni/Constants/Constants.dart';
+import 'package:techuni/Models/QuestionModel.dart';
 import 'package:techuni/Models/UserModel.dart';
 
-class DatabaseServices {}
+class DatabaseServices {
+  static Future<QuerySnapshot> getAllUsers() async {
+    Future<QuerySnapshot> users = usersRef.get();
+
+    return users;
+  }
+
+  Future<void> createQuestionData(QuestionModel question) async {
+    final data = <String, dynamic>{
+      'title': question.title,
+      'comment': question.comment,
+      'technology': question.technology,
+      'environment': question.environment,
+    };
+
+    await questionsRef
+        .add(
+          data,
+        )
+        .catchError(
+          (Object error) => print('Failed to set data: ${error.toString()}'),
+        );
+  }
+}
