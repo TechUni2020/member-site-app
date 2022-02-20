6817e55a24ac209d1c65dc711763976711b4b941
井手翔陽 <shouhi605@gmail.com>
2022-02-20 12:34:11 +0900

    add Home Widget

---
+++ lib/Widgets/Home/UserIcon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:techuni/Widgets/Home/IconFont.dart';
+
+class UserIcon extends StatelessWidget {
+  Color? color;
+  String? iconName;
+  double size;
+  double padding;
+
+  UserIcon({this.color, this.iconName, this.size = 30, this.padding = 10});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Container(
+          color: this.color,
+          padding: EdgeInsets.all(this.padding),
+          child: IconFont(
+              color: Colors.white, iconName: this.iconName, size: this.size)),
+    );
+  }
+}
