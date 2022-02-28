523ae3f5d5074bfacb15f2c21b66038cf87869c3
井手翔陽 <shouhi605@gmail.com>
2022-03-01 04:44:43 +0900

    fix FeedScreen

--- lib/Screens/FeedScreen.dart
+++ lib/Screens/FeedScreen.dart
@@ -45,83 +45,67 @@ class _FeedScreenState extends State<FeedScreen> {
 
   @override
   void initState() {
+    print(currentUserId);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final double iconSize = 50;
     return Scaffold(
       body: IndexedStack(
         index: _currentIndex,
         children: _pages,
       ),
-      bottomNavigationBar: Container(
-        height: 120,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bottombar/bottom_background.png'),
-            fit: BoxFit.fill,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        currentIndex: _currentIndex,
+        onTap: _setBottomBarIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            // backgroundColor: Palette.lightgray,
+            icon: Image.asset('assets/bottombar/unselected.png',
+                height: iconSize),
+            activeIcon:
+                Image.asset('assets/bottombar/Home.png', height: iconSize),
+            label: 'Home',
           ),
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          currentIndex: _currentIndex,
-          onTap: _setBottomBarIndex,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                  height: 50,
-                  child:
-                      Image.asset('assets/bottombar/Hedgehog_unselected.png')),
-              activeIcon: SizedBox(
-                  height: 50,
-                  child: Image.asset('assets/bottombar/Hedgehog_selected.png')),
-              label: 'Home',
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/bottombar/unselected.png',
+              height: iconSize,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              activeIcon: Icon(Icons.business_center),
-              label: ' Products',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              activeIcon: Icon(Icons.school_outlined),
-              label: '  Calender',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              activeIcon: Icon(Icons.settings_accessibility),
-              label: ' Questions',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              activeIcon: Icon(Icons.settings_accessibility),
-              label: '  MyPage',
-            ),
-          ],
-
-          // type: BottomNavigationBarType.shifting,
-          // // ここで色を設定していても、shiftingにしているので
-          // // Itemの方のbackgroundColorが勝ちます。
-          // enableFeedback: true,
-          // // IconTheme系統の値が優先されます。
-          // iconSize: 18,
-          // // 横向きレイアウトは省略します。
-          // // landscapeLayout: 省略
-          // selectedFontSize: 20,
-          // selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
-          // selectedLabelStyle: const TextStyle(color: Colors.red),
-          // // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
-          // selectedItemColor: Colors.black,
-          // unselectedFontSize: 15,
-          // unselectedIconTheme:
-          //     const IconThemeData(size: 25, color: Colors.white),
-          // unselectedLabelStyle: const TextStyle(color: Colors.purple),
-          // // IconTheme系統の値が優先されるのでこの値は適応されません。
-          // unselectedItemColor: Colors.red,
-        ),
+            activeIcon:
+                Image.asset('assets/bottombar/Product.png', height: iconSize),
+            label: ' Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/bottombar/unselected.png',
+                height: iconSize),
+            activeIcon:
+                Image.asset('assets/bottombar/Calender.png', height: iconSize),
+            label: '  Calender',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/bottombar/unselected.png',
+                height: iconSize),
+            activeIcon:
+                Image.asset('assets/bottombar/Question.png', height: iconSize),
+            label: ' Questions',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/bottombar/unselected.png',
+                height: iconSize),
+            activeIcon:
+                Image.asset('assets/bottombar/Mypage.png', height: iconSize),
+            label: '  MyPage',
+          ),
+        ],
+        type: BottomNavigationBarType.shifting,
+        enableFeedback: true,
+        iconSize: 18,
+        selectedFontSize: 14,
+        selectedItemColor: Colors.black,
       ),
     );
   }
